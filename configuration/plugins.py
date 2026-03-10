86f11adb966e6931e86c0672e87921152f57e119
XYG <xyg@terateams.com>
2026-03-10 18:50:07 +0800

    更新 Dockerfile-Plugins，添加版本参数；在 plugins.py 中引入数据库配置和插件；新增 env.sample 文件；更新插件需求列表以包含 netboxlabs-netbox-branching

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,10 +4,33 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-PLUGINS = ["netbox_contract"]
+from os import environ
+
+from netbox_branching.utilities import DynamicSchemaDict
+
+PLUGINS = [
+    "netbox_contract",
+    "netbox_branching",
+]
 
 # PLUGINS_CONFIG = {
 #   "netbox_bgp": {
 #     ADD YOUR SETTINGS HERE
 #   }
 # }
+
+DATABASES = DynamicSchemaDict({
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get("DB_NAME", "netbox"),         # Database name
+        'USER': environ.get("DB_USER", ""),               # PostgreSQL username
+        'PASSWORD': environ.get("DB_PASSWORD", ""),       # PostgreSQL password
+        'HOST': environ.get("DB_HOST", "postgres"),       # Database server
+        'PORT': '',                                       # Database port (leave blank for default)
+        'CONN_MAX_AGE': 300,                              # Max database connection age
+    }
+})
+
+DATABASE_ROUTERS = [
+    "netbox_branching.database.BranchAwareRouter",
+]
